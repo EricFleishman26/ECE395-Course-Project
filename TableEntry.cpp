9a56ab24de3ff31aad5e6427e831804430597c7f
ericfleishman <ericfleishmanwork@gmail.com>
2022-10-19 20:12:01 -0400

    Most of Instruction Buffer Implemented

--- TableEntry.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "TableEntry.h"
-
-TableEntry::TableEntry(int location, int length) {
-    tuple <int, int> temp(location, length);
-    entry = temp;
-}
